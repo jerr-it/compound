96168252e84f24affdf0aa0ae25b1bd0b82e4afb
cherrysrc <50867655+cherrysrc@users.noreply.github.com>
2021-04-06 21:54:54 +0200

    Made message viewer stateless

--- lib/pages/messages/messageViewer.dart
+++ lib/pages/messages/messageViewer.dart
@@ -2,16 +2,11 @@ import 'package:fludip/provider/messages/messageModel.dart';
 import 'package:fludip/util/str.dart';
 import 'package:flutter/material.dart';
 
-class MessageViewer extends StatefulWidget {
-  Message _message;
+class MessageViewer extends StatelessWidget {
+  final Message _message;
 
   MessageViewer({@required message}) : _message = message;
 
-  @override
-  _MessageViewerState createState() => _MessageViewerState();
-}
-
-class _MessageViewerState extends State<MessageViewer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,23 +15,15 @@ class _MessageViewerState extends State<MessageViewer> {
         child: ListView(
           padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
           children: [
-            Text(
-              widget._message.subject,
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            Divider(),
             ListTile(
               leading: Image.network(
-                widget._message.sender.avatarUrlNormal,
+                _message.sender.avatarUrlNormal,
                 width: 30,
                 height: 30,
               ),
-              title: Text(widget._message.sender.email),
+              title: Text(_message.sender.email),
               subtitle: Text(
-                StringUtil.fromUnixTime(widget._message.mkdate * 1000, "dd.MM.yyyy HH:mm"),
+                StringUtil.fromUnixTime(_message.mkdate * 1000, "dd.MM.yyyy HH:mm"),
                 style: TextStyle(
                   fontWeight: FontWeight.w300,
                 ),
@@ -44,7 +31,14 @@ class _MessageViewerState extends State<MessageViewer> {
             ),
             Divider(),
             Text(
-              StringUtil.removeHTMLTags(widget._message.content),
+              _message.subject,
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            Text(
+              StringUtil.removeHTMLTags(_message.content),
             ),
           ],
         ),
