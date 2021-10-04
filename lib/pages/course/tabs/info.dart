c682ab45aeff5124dc5ccdaa36fad6b9f7cdb0b0
cherrysrc <50867655+cherrysrc@users.noreply.github.com>
2021-10-04 12:50:59 +0200

    Added general course info

--- lib/pages/course/tabs/info.dart
+++ lib/pages/course/tabs/info.dart
@@ -10,10 +10,62 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:easy_localization/easy_localization.dart';
 
+class InfoLineWidget extends StatelessWidget {
+  InfoLineWidget(String key, String value)
+      : _key = key,
+        _value = value;
+
+  final String _key;
+  final String _value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              _key,
+              style: GoogleFonts.montserrat(fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+              child: Text(
+                _value ?? "nothing".tr(),
+                style: GoogleFonts.montserrat(),
+                textAlign: TextAlign.end,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
 class InfoTab extends StatelessWidget {
   final Course course;
 
   InfoTab({@required course}) : course = course;
+
+  List<Widget> _gatherInfo() {
+    List<Widget> widgets = <Widget>[];
+
+    widgets.add(InfoLineWidget("subtitle".tr(), course.subtitle));
+    widgets.add(InfoLineWidget("number".tr(), course.number));
+    widgets.add(InfoLineWidget("description".tr(), course.description));
+    widgets.add(InfoLineWidget("location".tr(), course.location));
+    widgets.add(InfoLineWidget("start".tr(), course.startSemester.title));
+    widgets.add(InfoLineWidget("end".tr(), course.endSemester.title));
+    widgets.add(InfoLineWidget("announcements".tr(), ""));
+
+    return widgets;
+  }
 
   ///Helper to get list of announcements from this course
   List<Widget> _gatherAnnouncements(List<News> news) {
@@ -38,7 +90,7 @@ class InfoTab extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("news".tr() + ": " + course.title, style: GoogleFonts.montserrat()),
+        title: Text("info".tr() + ": " + course.title, style: GoogleFonts.montserrat()),
         backgroundColor: course.color,
       ),
       body: FutureBuilder(
@@ -52,7 +104,7 @@ class InfoTab extends StatelessWidget {
                 ),
                 child: ListView(
                   padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                  children: _gatherAnnouncements(snapshot.data),
+                  children: _gatherInfo() + _gatherAnnouncements(snapshot.data),
                 ),
               ),
               onRefresh: () async {
